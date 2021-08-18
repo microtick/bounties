3f66cb2090914eefade63f5d0de932811bb586e8
Raul Bernal <bernal.raul@gmail.com>
2021-08-18 11:29:17 +0200

    Create statesync_client.sh

---
+++ statesync/mainnet_implementation/statesync_client.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Based on the work of Joe (Chorus-One) for Microtick - https://github.com/microtick/bounties/tree/main/statesync
+# You need config in two peers (avoid seed servers) this values in app.toml:
+#     [state-sync]
+#     snapshot-interval = 1000
+#     snapshot-keep-recent = 10
+# Pruning should be fine tuned also, for this testings is set to nothing
+#     pruning = "nothing"
+
+set -e
+
+# Change for your custom chain
+BINARY="https://microtick.com/releases/mainnet/microtick-v2.0.2-linux-x86_64.tar.gz"
+GENESIS="https://raw.githubusercontent.com/microtick/stargate-genesis/master/genesis.json"
+APP="Microtick: ~/.microtick"
+
+read -p "$APP folder, your keys and config will be erased, proced (y/n)? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # Microtick State Sync client config.
+  rm -f microtick-v2.0.2-linux-x86_64.tar.gz #deletes a previous downloaded tar
+  rm -f mtm #deletes a previous downloaded binary
+  rm -rf $HOME/.microtick/ #deletes previous installation   
+  wget -nc $BINARY
+  tar -xzvf microtick-v2.0.2-linux-x86_64.tar.gz 
+  chmod +x mtm
+  ./mtm init New_peer --chain-id microtick-1
+  rm -rf $HOME/./microtick/config/genesis.json #deletes the default created genesis
+  curl -s $GENESIS > $HOME/.microtick/config/genesis.json
+  
+  NODE1_IP="178.128.247.173"
+  RPC1="http://$NODE1_IP"
+  P2P_PORT1=26656
+  RPC_PORT1=26657
+
+  NODE2_IP="159.65.198.245"
+  RPC2="http://$NODE2_IP"
+  RPC_PORT2=26657
+  P2P_PORT2=26656
+
+  #If you want to use a third StateSync Server... 
+  #DOMAIN_3=microtick.zone     # If you want to use domain names 
+  #NODE3_IP=$(dig $DOMAIN_1 +short
+  #RPC3="http://$NODE3_IP"
+  #RPC_PORT3=26657
+  #P2P_PORT3=26656
+
+  INTERVAL=1000
+
+  LATEST_HEIGHT=$(curl -s $RPC1:$RPC_PORT1/block | jq -r .result.block.header.height);
+  BLOCK_HEIGHT=$((($(($LATEST_HEIGHT / $INTERVAL)) -10) * $INTERVAL)); #Mark addition
+  
+  if [ $BLOCK_HEIGHT -eq 0 ]; then
+    echo "Error: Cannot state sync to block 0; Latest block is $LATEST_HEIGHT and must be at least $INTERVAL; wait a few blocks!"
+    exit 1
+  fi
+
+  TRUST_HASH=$(curl -s "$RPC1:$RPC_PORT1/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+  if [ "$TRUST_HASH" == "null" ]; then
+    echo "Error: Cannot find block hash. This shouldn't happen :/"
+    exit 1
+  fi
+
+  NODE1_ID=$(curl -s "$RPC1:$RPC_PORT1/status" | jq -r .result.node_info.id)
+  NODE2_ID=$(curl -s "$RPC2:$RPC_PORT2/status" | jq -r .result.node_info.id)
+  #NODE3_ID=$(curl -s "$RPC3:$RPC_PORT3/status" | jq -r .result.node_info.id)
+
+
+  sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+  s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"http://$NODE1_IP:$RPC_PORT1,http://$NODE2_IP:$RPC_PORT2\"| ; \
+  s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+  s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
+  s|^(persistent_peers[[:space:]]+=[[:space:]]+).*$|\1\"${NODE1_ID}@${NODE1_IP}:${P2P_PORT1},${NODE2_ID}@${NODE2_IP}:${P2P_PORT2}\"| ; \
+  s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"d6aa4c9f3ccecb0cc52109a95962b4618d69dd3f@seed1.bitcanna.io:26656,23671067d0fd40aec523290585c7d8e91034a771@seed2.bitcanna.io:16656\"|" $HOME/.microtick/config/config.toml
+
+ 
+  sed -E -i 's/minimum-gas-prices = \".*\"/minimum-gas-prices = \"0.025utick\"/' $HOME/.microtick/config/app.toml
+
+  ./bcnad unsafe-reset-all
+  ./bcnad start
+   echo If your node is synced considerate to create a service file. 
+fi
